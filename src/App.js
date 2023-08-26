ebf899ad9f79a9526a1ad91bc69fb56e7fd7ba65
bin2159 <bipinthms007@gmail.com>
2023-08-26 15:51:18 +0530

    firebase backend without thunk and request notification

--- src/App.js
+++ src/App.js
@@ -1,16 +1,72 @@
-import { useSelector } from 'react-redux';
-import Cart from './components/Cart/Cart';
-import Layout from './components/Layout/Layout';
-import Products from './components/Shop/Products';
+import { useDispatch, useSelector } from "react-redux";
+import Cart from "./components/Cart/Cart";
+import Layout from "./components/Layout/Layout";
+import Products from "./components/Shop/Products";
+import { useEffect } from "react";
+import { cartActions } from "./store/cart";
+import Notification from "./components/UI/Notification";
+
+let initial = true;
 
 function App() {
-  const showCart=useSelector(state=>state.cart.showCart)
+  
+  const dispatch = useDispatch();
+  const showCart = useSelector((state) => state.cart.showCart);
+  const cart = useSelector((state) => state.cartItem)
+  const notification = useSelector((state) => state.cart.notification);
 
+  useEffect(() => {
+    const sendCartData = async () => {
+      dispatch(
+        cartActions.setNotification({
+          status: "sending",
+          title: "Sending",
+          message: "Sending cart data",
+        })
+      );
+
+      const response = await fetch(
+        "https://react-redux-e9917-default-rtdb.firebaseio.com/cart.json",
+        {
+          method: "PUT",
+          body: JSON.stringify(cart),
+          headers: { "Content-Type": "application/json" },
+        }
+      );
+      if (!response.ok) {
+        throw new Error("Error Sending data");
+      }
+      dispatch(
+        cartActions.setNotification({
+          status: "success",
+          title: "Success",
+          message: "Cart data send",
+        })
+      );
+    };
+
+    if (initial) {
+      initial = false;
+      return;
+    }
+    sendCartData().catch((error) => {
+      dispatch(
+        cartActions.setNotification({
+          status: "error",
+          title: "Error",
+          message: error,
+        })
+      );
+    });
+  }, [cart, dispatch]);
   return (
-    <Layout>
-     {showCart&& <Cart />}
-      <Products />
-    </Layout>
+    <>
+      {notification && <Notification status={notification.status} title={notification.title} message={notification.message}/>}
+      <Layout>
+        {showCart && <Cart />}
+        <Products />
+      </Layout>
+    </>
   );
 }
 
--- src/store/cart.js
+++ src/store/cart.js
@@ -1,14 +1,21 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const initialState={showCart:false}
-const cartSlice=createSlice({
-    name:"cart",
-    initialState,
-    reducers:{
-        setShowCart(state){
-            state.showCart=!state.showCart
-        }
-    }
-})
-export default cartSlice.reducer
-export const cartActions=cartSlice.actions
+const initialState = { showCart: false, notification: null };
+const cartSlice = createSlice({
+  name: "cart",
+  initialState,
+  reducers: {
+    setShowCart(state) {
+      state.showCart = !state.showCart;
+    },
+    setNotification(state, action) {
+      const data = action.payload;
+      console.log(data)
+      state.notification = {
+       ...data
+      };
+    },
+  },
+});
+export default cartSlice.reducer;
+export const cartActions = cartSlice.actions;
